675aece82d70ba5883f193e753b6772b58097b1c
Arsham Mesbah <arsham@microsoft.com>
2015-02-01 00:23:55 -0800

    Adding Min, Max and Abs methods.

--- BitTWHacks/bithacks.cpp
+++ BitTWHacks/bithacks.cpp
@@ -1,20 +1,61 @@
 
 #include <iostream> 
 #include <bitset>
+#include <iomanip>
 #include "bithacks.h"
 using namespace std; 
 
 int main()
 {
-   int x = 10.2; 
-   bithacks::showbits(x);
+   /*int x = 10.2; 
+   bithacks::showbits(x);*/
 
+   /* int i = 'a';
+    char buffer[33];
+    _itoa_s(i, buffer, 2);
+    printf("binary: %s\n", buffer);*/
    
-   
+
+    //Tests cases for Min (Integral Only)
+    /*int x = 3, y = 4; 
+    std::cout<< "Min(" << x <<", " << y <<"): " << bithacks::Min(x, y) ; 
+
+    long e = 30, f = 40;
+    std::cout << "Min(" << e << ", " << f << "): " << bithacks::Min(e, f);*/
+
+    //Tests cases for Abs
+   /* int x = -1000; 
+    cout << bithacks::Abs(x) <<endl; 
+
+    int y = 200; 
+    cout << bithacks::Abs(y) << endl;*/
+
+
     system("PAUSE");
     return 0;
 }
-   
+  
+
+
+//void toBinary(float *num) {
+//    char* numi = reinterpret_cast<char*>(num);
+//    cout << num << " " << numi << endl;
+//    
+//    for (int i = 8 * sizeof(num) ; i < length; i++)
+//    {
+//
+//    }
+//    
+//    for (int i = 0; i < 8 * sizeof(num); i++){
+//        if (numi & (1 << i)) {
+//            cout << 1;
+//        }
+//        else {
+//            cout << 0;
+//        }
+//    }
+//    cout << endl << endl;
+//}
 
     /*++
         Computes sign of a given integer.
@@ -27,7 +68,7 @@ int main()
 
     -1 if v < 0, else 0
 --*/
-int bithacks::ComputeSign_1(int v)
+int bithacks::ComputeSign(int v)
 {
     /* CHAR_BIT is the number of bits per byte (normally 8).
     *  to avoid branching on CPUs with flag registers (IA32)
@@ -70,10 +111,79 @@ int bithacks::ComputeSign_1(int v)
 
     -1 if v < 0, else 0
 --*/
-int bithacks::ComputeSign_2(int x)
+int bithacks::ComputeSign_BitMask(int x)
 {
     // The & will result either in 0x80000000 or 0x00000000 
     return (x & bithacks::bit_mask(x) ? -1 : 0);
+}
+
+/*++
+        Returns the smaller of two integral values.
+        
+        On some rare machines where branching is very expensive and no condition move 
+        instructions exist, the expression below might be faster than the obvious approach, 
+        r = (x < y) ? x : y, even though it involves two more instructions.
+        (Typically, the obvious approach is best, though.) It works because if x < y, 
+        then -(x < y) will be all **ones**, so r = y ^ (x ^ y) & ~0 = y ^ x ^ y = x. Otherwise, 
+        if x >= y, then -(x < y) will be all **zeros**, so r = y ^ ((x ^ y) & 0) = y. On some machines, 
+        evaluating (x < y) as 0 or 1 requires a branch instruction, so there may be no advantage.
+
+        If you know that INT_MIN <= x - y <= INT_MAX, then you can use the following, 
+        which are faster because (x - y) only needs to be evaluated once.
+        
+        r = y + ((x - y) & ((x - y) >> (sizeof(int) * CHAR_BIT - 1))); // min(x, y)
+        r = x - ((x - y) & ((x - y) >> (sizeof(int) * CHAR_BIT - 1))); // max(x, y)
+
+    Arguments:
+
+        x - An arguement (integral).
+        y - Another argument (integral).
+
+    Return Value:
+
+        The smaller of X and Y.
+--*/
+template<class T> std::enable_if_t<std::is_integral<T>::value, T>
+    bithacks::Min(const T& x, const T& y)
+{
+    return y ^ ((x ^ y) & -(x < y)); // min(x, y)
+}
+
+/*++
+        Returns the larger of two integral values.
+
+    Arguments:
+
+        x - An arguement (integral).
+        y - Another argument (integral).
+
+    Return Value:
+
+        The larger of X and Y.
+--*/
+template<class T> std::enable_if_t<std::is_integral<T>::value, T>
+    bithacks::Max(const T& x, const T& y)
+{
+    return x ^ ((x ^ y) & -(x < y)); // max(x, y)
+}
+
+/*++
+        Returns the absolute value of integral argument provided.
+
+    Arguments:
+
+        x - An arguement (integral).
+
+    Return Value:
+
+        Abslute value of X.
+--*/
+
+template<class T> std::enable_if_t<std::is_integral<T>::value, T>
+    bithacks::Abs(const T& x)
+{
+    int const mask = x >> sizeof(int) * CHAR_BIT - 1; 
+    return (x + mask) ^ mask;
 }
 
  /*++
@@ -87,11 +197,11 @@ int bithacks::ComputeSign_2(int x)
 
         bit mask with MSB set to 1.
 --*/
-template <typename T> T bithacks::bit_mask(T x)
+    template <typename T> std::enable_if_t<std::is_integral<T>::value, T> 
+        bithacks::bit_mask(T x)
 {
     return 1 << (sizeof(x) * CHAR_BIT) - 1;
 }
-
 
 /*++
         Prints out bit values 0s and 1s of a given integer.
--- BitTWHacks/bithacks.h
+++ BitTWHacks/bithacks.h
@@ -4,10 +4,23 @@
 class bithacks
 {
 public:
-    static int ComputeSign_1(int v);
-    static int ComputeSign_2(int v);
-    template <typename T> static void showbits(T x);
-    template <typename T> static T bit_mask(T x);
-private:
+    static int ComputeSign(int v);
+    static int ComputeSign_BitMask(int v);
+    
+    template<class T>
+    std::enable_if_t<std::is_integral<T>::value, T>
+        static Min(const T& x, const T& y); 
+    
+    template<class T>
+    std::enable_if_t<std::is_integral<T>::value, T>
+        static Max(const T& x, const T& y);
 
+    template<class T>
+    std::enable_if_t<std::is_integral<T>::value, T>
+        static Abs(const T& x);
+
+    template <typename T> static void showbits(T x);
+   
+    template <typename T> std::enable_if_t<std::is_integral<T>::value, T> 
+        static bit_mask(T x);
 };
